50963e75ea81e5251fca0d73d6bb9267d4aada33
OleksandrPokotylenko <aln.nko.of@gmail.com>
2020-12-04 02:32:54 +0200

    done

--- src/main/java/core/basesyntax/UserService.java
+++ src/main/java/core/basesyntax/UserService.java
@@ -1,8 +1,19 @@
 package core.basesyntax;
 
+import core.basesyntax.exception.UserNotFoundException;
+
 public class UserService {
 
     public int getUserScore(String[] records, String email) {
-        return 0;
+        return findIndex(records, email);
+    }
+
+    private int findIndex(String[] records, String email) {
+        for (String record : records) {
+            if (record.contains(email) && !record.replace(email, "").isEmpty()) {
+                return Integer.parseInt(record.substring(record.indexOf(":") + 1));
+            }
+        }
+        throw new UserNotFoundException();
     }
 }
